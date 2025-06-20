61606e1bb937c319e81a20779decea5b876507f2
seongsik-17 <mss9810@naver.com>
2025-06-20 15:34:39 +0900

    [ssmoon]merge

--- oncall/src/main/java/com/example/oncall/controller/VeterinarianController.java
+++ oncall/src/main/java/com/example/oncall/controller/VeterinarianController.java
@@ -1,20 +1,33 @@
 package com.example.oncall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.oncall.entity.Veterinarian;
+import com.example.oncall.service.VeterinarianService;
+
 @Controller
 public class VeterinarianController {
+
+    
+	@Autowired
+	VeterinarianService veterinarianService;
+
 	@GetMapping("/counsel")
 	public String getCounsel() {
 		return "counsel";
 	}
 	
 	@PostMapping("/counsel")
-	public String counsel(@RequestParam("username") String username) {
+	public String counsel(@RequestParam("username") String username,Model model) {
 		System.out.println(username);
+		
+		model.addAttribute("veterinarians", veterinarianService.getVetByUsername(username));
+		
 		return "redirect:/counsel";
 	}
 }
--- oncall/src/main/java/com/example/oncall/service/VeterinarianService.java
+++ oncall/src/main/java/com/example/oncall/service/VeterinarianService.java
@@ -1,6 +1,7 @@
 package com.example.oncall.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,9 @@ public class VeterinarianService {
 	
 	public List<Veterinarian> getAllVet() {
 	    return veterinarianRepository.findAll();
+	}
+	
+	public Optional<Veterinarian> getVetByUsername(String username) {
+		return veterinarianRepository.findById(username);
 	}
 }
