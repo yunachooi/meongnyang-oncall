51e29f6e1bd9ebd22bdef3af0637dbc27ad13848
yunachooi <glasodyyn@gmail.com>
2025-06-20 12:36:48 +0900

    [ynchoi] veterinarian, index

--- oncall/src/main/java/com/example/oncall/controller/MainController.java
+++ oncall/src/main/java/com/example/oncall/controller/MainController.java
@@ -1,13 +1,24 @@
 package com.example.oncall.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.oncall.entity.Veterinarian;
+import com.example.oncall.service.VeterinarianService;
 
 @Controller
 public class MainController {
+	@Autowired
+    private VeterinarianService veterinarianService;
 	
 	@GetMapping("/")
-	public String root() {
+	public String root(Model model) {
+		List<Veterinarian> vets = veterinarianService.getAllVet();
+	    model.addAttribute("veterinarians", vets);
 		return "index";
 	}
 	
