7846ef31c92d0248a50fee78374043f80dbf8c8d
seongsik-17 <mss9810@naver.com>
2025-06-20 13:49:45 +0900

    [ssmoon] Task UserRepository

--- oncall/src/main/java/com/example/oncall/repository/UserRepository.java
+++ oncall/src/main/java/com/example/oncall/repository/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.oncall.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String>{
-	String findPasswordById(String username);
+
+	User findByUsername(String username);
 }
