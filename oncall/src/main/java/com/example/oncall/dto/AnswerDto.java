aa1cceb0fa01d996a1b661de044b88fa18f43e04
yunachooi <glasodyyn@gmail.com>
2025-06-20 11:16:00 +0900

    [common] init

---
+++ oncall/src/main/java/com/example/oncall/dto/AnswerDto.java
@@ -0,0 +1,5 @@
+package com.example.oncall.dto;
+
+public class AnswerDto {
+
+}
---
+++ oncall/src/test/java/com/example/oncall/OncallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oncall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
